4feaa73017629f43e76e44404510ccfbb5be1fcf
l-iberty <2593093242@qq.com>
2016-03-11 23:23:58 +0800

    ..

--- bitman/Directory/Directory.java
+++ bitman/Directory/Directory.java
@@ -1,19 +1,26 @@
 import java.util.*;
 
 public class Directory{
-	static int choice;
-	static String inName;
-	static String inTelephone;
 	static ArrayList<Person> personList = new ArrayList<>();
 	
 	public static void main(String[] arg){
+		Tips tips = new Tips();
+		tips.showTips();
+	}
+}
+
+class Tips{
+	private int choice;
+	private ManagerChoice mce = new ManagerChoice();
+	
+	public void showTips(){
 		System.out.println("-----------Directory Manager------------");
 		System.out.println("<1> Add to Directory");
 		System.out.println("<2> Delete Directory");
 		System.out.println("<3> Search Directory");
 		System.out.println("<4> Show Directory");
 		System.out.println("<5> Back");
-		
+			
 		while(true){
 			System.out.println("------------------------------------------");
 			System.out.println("\n>>> Please enter 0-5,others are invalid.>>>");
@@ -26,163 +33,167 @@ public class Directory{
 			}
 			
 			if(choice != 5){
-				managerChoice(choice);
+				mce.managerChoice(choice);
 				choice = -1; /** If I don't do this,there will be a singular BUG
-				                 when the user enter something unexpected.
-							  **/
+								 when the user enters something unexpected. **/
 			}
 			else
 				break;
 		}
 	}
 	
-	static void managerChoice(int inChoice){
-		int tmpChoice = -1;
-		switch(inChoice){
-			case 1:
-				addInformation();
-				break;
+	private class ManagerChoice{
+	
+		public void managerChoice(int inChoice){
+			switch(inChoice){
+				case 1:
+					addInformation();
+					break;
 		
-			case 2:
-				delInformation();
-				break;
+				case 2:
+					delInformation();
+					break;
 			
-			case 3:
-				searchInformation();
-				break;
+				case 3:
+					searchInformation();
+					break;
 				
-			case 4:
-				if(personList.size() == 0)
-					System.out.println(">> No Record in the Directory");
+				case 4:
+					if(Directorys.personList.size() == 0)
+						System.out.println(">> No Record in the Directory");
+					else
+						showInformation();
+				
+					break;
+			}
+		}
+		
+		private void addInformation(){
+			String inName;
+			String inTelephone;
+	
+			System.out.print("\nPlease enter the name: ");
+			Scanner getName = new Scanner(System.in);
+			inName = getName.nextLine();
+			
+			System.out.print("Please enter the telephone number: ");
+			Scanner getTel = new Scanner(System.in);
+			inTelephone = getTel.nextLine();
+				
+			Person ref = new Person(inName, inTelephone);
+			Directorys.personList.add(ref);
+			
+			if(Directorys.personList.size() > 0){
+				System.out.println("------------------------------------------");
+				System.out.println("Done Successfully!");
+			}
+			else{
+				System.out.println("------------------------------------------");
+				System.out.println("Fail to add information!");
+			}
+		}
+		
+		private void delInformation(){
+			int tmpChoice = -1;
+		
+			if(Directorys.personList.size() == 0)
+				System.out.println(">> No Record in the Directory");
+			else{
+				System.out.println("------------------------------------------");
+				System.out.println("\nAll the person's information is as following,"+
+						"you can enter the order number to delete the matching one."+
+						"If you need to delete all the records,enter \"0\"."+
+						"If you are meant to return,please enter any character other than the order number and \"0\".");
+				System.out.println("------------------------------------------");
+			
+				showInformation();
+				System.out.println("");
+			
+				try{
+					Scanner getTmpChoice = new Scanner(System.in);
+					tmpChoice = getTmpChoice.nextInt();
+				}catch(InputMismatchException e){
+					;
+				}
+				
+				if(tmpChoice == 0){
+					Directorys.personList.clear();
+					System.out.println(">> Deleted! Enter \"4\" to check the result.");
+				}
+				else if(tmpChoice-1 < Directorys.personList.size() && tmpChoice-1 >= 0){
+					Directorys.personList.remove(tmpChoice-1);
+					System.out.println(">> Deleted! Enter \"4\" to check the result.");
+				}
 				else
-					showInformation();
-			
-				break;
+					;
+			}
 		}
-	}
-	
-	static void addInformation(){
-		System.out.print("\nPlease enter the name: ");
-				Scanner getName = new Scanner(System.in);
-				inName = getName.nextLine();
-			
-		System.out.print("Please enter the telephone number: ");
-		Scanner getTel = new Scanner(System.in);
-		inTelephone = getTel.nextLine();
-				
-		Person ref = new Person(inName, inTelephone);
-		personList.add(ref);
-			
-		if(personList.size() != 0){
-			System.out.println("------------------------------------------");
-			System.out.println("Done Successfully!");
-		}
-		else{
-			System.out.println("------------------------------------------");
-			System.out.println("Fail to add information!");
-		}
-	}
-	
-	static void delInformation(){
-		int tmpChoice = -1;
 		
-		if(personList.size() == 0)
-			System.out.println(">> No Record in the Directory");
-		else{
-			System.out.println("------------------------------------------");
-			System.out.println("\nAll the person's information is as following,"+
-					"you can enter the order number to delete the matching one."+
-					"If you need to delete all the records,enter \"0\"."+
-					"If you are meant to return,please enter any character other than the order number and \"0\".");
-			System.out.println("------------------------------------------");
-			
-			showInformation();
-			System.out.println("");
-			
-			try{
-				Scanner getTmpChoice = new Scanner(System.in);
-				tmpChoice = getTmpChoice.nextInt();
-			}catch(InputMismatchException e){
-				;
-			}
-				
-			if(tmpChoice == 0){
-				personList.clear();
-				System.out.println(">> Deleted! Enter \"4\" to check the result.");
-			}
-			else if(tmpChoice-1 < personList.size() && tmpChoice-1 >= 0){
-				personList.remove(tmpChoice-1);
-				System.out.println(">> Deleted! Enter \"4\" to check the result.");
-			}
-			else
-				;
-		}
-	}
-	
-	static void searchInformation(){
-		String tmpName;
-		String tmpTel;
-		String search;
-		int j; // j means the index of the element
-		int count; /** each time the program finds a matching element
-                       the variable count will increase by 1.					
-		            **/
-		if(personList.size() == 0){
-			System.out.println(">> No Record in the Directory");
-		}else{
-			System.out.println("------------------------------------------");
-			System.out.println("<A> Enter \"A\" to search via name\n"+
-						"<B> Enter \"B\" to search via telephone number");
-			search = new Scanner(System.in).nextLine();
+		private void searchInformation(){
+			String tmpName;
+			String tmpTel;
+			String search;
+			int j; // j means the index of the element
+			int count; /** each time the program finds a matching element
+						   the variable count will increase by 1. **/
+						   
+			if(Directorys.personList.size() == 0){
+				System.out.println(">> No Record in the Directory");
+			}else{
+				System.out.println("------------------------------------------");
+				System.out.println("<A> Enter \"A\" to search via name\n"+
+								   "<B> Enter \"B\" to search via telephone number");
+				search = new Scanner(System.in).nextLine();
 							
-			if(search.equals("a") || search.equals("A")){
-				System.out.print("Please enter the name to search: ");
-				tmpName = new Scanner(System.in).nextLine();
+				if(search.equals("a") || search.equals("A")){
+					System.out.print("Please enter the name to search: ");
+					tmpName = new Scanner(System.in).nextLine();
 				
-				for(j = 0, count = 0; j < personList.size(); j++){
-					Person tmp = personList.get(j);
+					for(j = 0, count = 0; j < Directorys.personList.size(); j++){
+						Person tmp = Directorys.personList.get(j);
 					
-					if(tmp.name.equals(tmpName)){
-						count++;
-						System.out.println("------------------------------------------");
-						System.out.println("\nSearching...");
-						System.out.println("Name: " + tmp.name);
-						System.out.println("Telephone Number: " + tmp.telephone);
+						if(tmp.name.equals(tmpName)){
+							count++;
+							System.out.println("------------------------------------------");
+							System.out.println("\nSearching...");
+							System.out.println("Name: " + tmp.name);
+							System.out.println("Telephone Number: " + tmp.telephone);
+						}
 					}
+					if(count == 0)
+						System.out.println("Not Found!");
 				}
-				if(count == 0)
-					System.out.println("Not Found!");
-			}
-			else if(search.equals("b") || search.equals("B")){
-				System.out.print("Please enter the telephone number to search: ");
-				tmpTel = new Scanner(System.in).nextLine();
+				else if(search.equals("b") || search.equals("B")){
+					System.out.print("Please enter the telephone number to search: ");
+					tmpTel = new Scanner(System.in).nextLine();
 				
-				for(j = 0, count = 0; j < personList.size(); j++){
-					Person tmp = personList.get(j);
+					for(j = 0, count = 0; j < Directorys.personList.size(); j++){
+						Person tmp = Directorys.personList.get(j);
 					
-					if(tmp.telephone.equals(tmpTel)){
-						count++;
-						System.out.println("------------------------------------------");
-						System.out.println("\nSearching...");
-						System.out.println("Name: " + tmp.name);
-						System.out.println("Telephone Number: " + tmp.telephone);
+						if(tmp.telephone.equals(tmpTel)){
+							count++;
+							System.out.println("------------------------------------------");
+							System.out.println("\nSearching...");
+							System.out.println("Name: " + tmp.name);
+							System.out.println("Telephone Number: " + tmp.telephone);
+						}
 					}
+					if(count == 0)
+						System.out.println("Not Found!");
 				}
-				if(count == 0)
-					System.out.println("Not Found!");
+				else
+					System.out.println(">> Invalid Input!Unable to search!");
 			}
-			else
-				System.out.println(">> Invalid Input!Unable to search!");
 		}
-	}
-	
-	static void showInformation(){
-		System.out.print("\n------------------------------------------");
-		for(int i = 0; i < personList.size(); i++){
-			Person tmp = personList.get(i);
-			System.out.println("\nPerson " + (i+1) + ":");
-			System.out.println("Name: " + tmp.name);
-			System.out.println("Telephone Number: " + tmp.telephone);
+		
+		private void showInformation(){
+			System.out.print("\n------------------------------------------");
+			for(int i = 0; i < Directorys.personList.size(); i++){
+				Person tmp = Directorys.personList.get(i);
+				System.out.println("\nPerson " + (i+1) + ":");
+				System.out.println("Name: " + tmp.name);
+				System.out.println("Telephone Number: " + tmp.telephone);
+			}
 		}
 	}
 }
@@ -191,7 +202,7 @@ class Person{
 	String name;
 	String telephone;
 	
-	Person(String inName, String inTelephone){
+	public Person(String inName, String inTelephone){
 		name = inName;
 		telephone = inTelephone;
 	}
