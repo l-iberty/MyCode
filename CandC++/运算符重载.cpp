6d160fd83a89b95d1a2b6a6620b6876ee00a26d8
l-iberty <2593093242@qq.com>
2016-05-17 13:53:41 +0800

    ...

---
+++ CandC++/运算符重载.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+	double real;
+	double image;
+
+public:
+	Complex(double real, double image) {
+		this->real = real;
+		this->image = image;
+	}
+
+	void display() {
+		cout << "(" << real << "," << image << ")" << endl;
+	}
+	/* 运算符重载有两种形式：重载为类的成员函数和类的非成员函数，
+	非成员函数通常是友元. 运算符要重载为类的友元函数，因为这样可
+	以自由访问类的任何数据成员. */
+	friend Complex operator + (Complex A, Complex B); // 重载+为友元函数
+	friend Complex operator + (Complex A, int a);
+	friend Complex operator - (Complex A, Complex B); // 重载-为友元函数
+	friend Complex operator - (Complex &A); // 取负
+	friend Complex operator ++ (Complex &A); // 前置++
+	friend Complex operator ++ (Complex &A, int); // 后置++
+};
+
+Complex operator + (Complex A, Complex B) {
+	return Complex(A.real + B.real, A.image + B.image);
+}
+
+Complex operator + (Complex A, int a) {
+	return Complex(A.real + a, A.image);
+}
+
+Complex operator - (Complex A, Complex B) {
+	return Complex(A.real - B.real, A.image - B.image);
+}
+
+Complex operator - (Complex &A) {
+	return Complex(-A.real, -A.image);
+}
+
+Complex operator ++ (Complex &A) {
+	return Complex(A.real++, A.image);
+}
+
+Complex operator ++ (Complex &A, int) {
+	return Complex(++A.real, A.image);
+}
+
+int main() {
+	Complex A(10, 20), B(4, 8);
+	Complex C = A + B;
+	cout << "A = ";
+	A.display();
+	cout << "B = ";
+	B.display();
+	cout << "C = A + B = ";
+	C.display();
+
+	C = A - B;
+	cout << "C = A - B = ";
+	C.display();
+
+	A++;
+	cout << "A++ = ";
+	A.display();
+
+	++B;
+	cout << "++B = ";
+	B.display();
+	
+	A = A + 100;
+	cout << "A = A + 100 = ";
+	A.display();
+
+	return 0;
+}
---
+++ CandC++/间接二义性.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using   namespace std;
+
+class Automobile {  //汽车类
+private:
+	int power;   //马力
+
+public:
+	Automobile(int power){
+		this->power = power;
+	}
+
+	void show(){
+		cout << "  power:" << power;
+	}
+};
+
+class Car :  public Automobile {  //小客车类
+private:
+	int seat;     //座位
+
+public:
+	Car(int power, int seat) :Automobile(power){
+		this->seat = seat;
+	}
+
+	void show(){
+		cout << "car:";
+		Automobile::show();
+		cout << "  seat:" << seat << endl;
+	}
+};
+
+class Wagon :  public Automobile {  //小货车类
+private:
+	int load;     //装载量
+
+public:
+	Wagon(int power, int load) : Automobile(power){
+		this->load = load;
+	}
+	void show(){
+		cout << "wagon:";
+		Automobile::show();
+		cout << "  load:" << load << endl;
+	}
+};
+
+class StationWagon :public Car, public Wagon  {  //客货两用车类
+public:
+	StationWagon(int CPower, int WPower, int seat, int load)
+		:Wagon(WPower, load), Car(CPower, seat){}
+
+	void show() {
+		cout << "StationWagon:" << endl;
+		Car::show();
+		Wagon::show();
+	}
+};
+
+
+int main() {
+	StationWagon SW(105, 108, 3, 8);
+	SW.show();
+	return 0;
+}
+// 基类 Automobile 的成员power在内存中有两个不同的拷贝，否则输出结果中两辆车的power为105
